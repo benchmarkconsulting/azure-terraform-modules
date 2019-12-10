bbdd12b050b3ccd68c37458f779493b46b67f06b
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-10 07:57:10 -0500

    updated nsg code

--- nsg/variables.tf
+++ nsg/variables.tf
@@ -1,46 +1,51 @@
-variable "name_prefix" {
-  type = string
-  description = "Machine prefix"
+# Network Security Group definition
+variable "resource_group_name" {
+  default     = "nsg_rg"
+  description = "Name of the resource group"
 }
 
-variable "amount" {
-  type        = string
-  description = "the amount of comute nodes to create"
-  default     = 1
-}
+variable "location" {}
 
-variable "machine_type" { 
-    type = string
-    description = "machine size"
-    default = "n1-standard-1"
-}
-
-variable "zone" {
-  type = string
-  description = "Zone ot deploy the machine in"
-  default = "us-east1-b"
-}
-
-variable "image" {
-    type = string
-    description = "OS image to use for compute instance"
-    default = "ubuntu-1904-disco-v20191019"
-}
-
-variable "subnetwork" {
-    type = string
-    description = "the network to host host compute instances"
-    default = "default"
+variable "security_group_name" {
+  description = "Network security group name"
+  default     = "nsg"
 }
 
 variable "tags" {
-    type = string
-    description = "the network to host host compute instances"
-    default = "default"
+  description = "The tags to associate with your network security group."
+  type        = "map"
+  default     = {}
 }
 
-variable "startup-script" {
-  type = string
-  description = "Name of the startup Script"
-  default = ""
+# Security Rules definition 
+
+# Predefined rules   
+variable "predefined_rules" {
+  type    = "list"
+  default = []
+}
+
+# Custom security rules
+# [priority, direction, access, protocol, source_port_range, destination_port_range, description]"
+# All the fields are required.
+variable "custom_rules" {
+  description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, description]"
+  type        = "list"
+  default     = []
+}
+
+# source address prefix to be applied to all rules
+variable "source_address_prefix" {
+  type    = "list"
+  default = ["*"]
+
+  # Example ["10.0.3.0/24"] or ["VirtualNetwork"]
+}
+
+# Destination address prefix to be applied to all rules
+variable "destination_address_prefix" {
+  type    = "list"
+  default = ["*"]
+
+  # Example ["10.0.3.0/32","10.0.3.128/32"] or ["VirtualNetwork"] 
 }
