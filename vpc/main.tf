e23748f558c1d169db0c3f5a638c061e5629897a
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-12-09 21:00:54 -0500

    updated module

--- vpc/main.tf
+++ vpc/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.subnet_names[count.index]}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  resource_group_name  = "${azurerm_resource_group.network.name}"
+  resource_group_name  = "${azurerm_resource_group.name}"
   address_prefix       = "${var.subnet_prefixes[count.index]}"
   count                = "${length(var.subnet_names)}"
 }
