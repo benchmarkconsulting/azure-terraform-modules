181c7cc56f41138657ba60ea2d3335a62dece79f
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-12-09 20:18:45 -0500

    updated module

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,23 @@
-resource "azurerm_virtual_network" "vpc-network" {
-  #count            = var
-  name             = var.network_name
-  address_space    = [var.address_space]
-  resource_group   = var.resource_group 
-  location         = var.location
-  }
+
+#Azure Generic vNet Module
+resource "azurerm_resource_group" "network" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vnet_name}"
+  location            = "${var.location}"
+  address_space       = ["${var.address_space}"]
+  resource_group_name = "${azurerm_resource_group.network.name}"
+  dns_servers         = "${var.dns_servers}"
+  tags                = "${var.tags}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.subnet_names[count.index]}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.network.name}"
+  address_prefix       = "${var.subnet_prefixes[count.index]}"
+  count                = "${length(var.subnet_names)}"
 }
