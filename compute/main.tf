508a2cf5e3c1c9f9016d89e02b0f7dd26fb8ef7f
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-10 10:46:13 -0500

    added compute section

---
+++ compute/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "nsg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_machine" "vm-linux" {
+  name                          = "${var.vm_hostname}${count.index}"
+  location                      = "${var.location}"
+  resource_group_name           = "${azurerm_resource_group.vm.name}"
+  availability_set_id           = "${azurerm_availability_set.vm.id}"
+  vm_size                       = "${var.vm_size}"
+  network_interface_ids         = ["${element(azurerm_network_interface.vm.*.id, count.index)}"]
+  delete_os_disk_on_termination = "${var.delete_os_disk_on_termination}"
