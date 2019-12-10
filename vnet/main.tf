e05cbd5ff5cd490fff20833eae79bd71ece5b734
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-09 20:02:03 -0500

    added vnet code

--- vnet/main.tf
+++ vnet/main.tf
@@ -10,9 +10,16 @@ resource "azurerm_virtual_network" "demo" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "frontend" {
-  name                 = "frontend"
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  virtual_network_name = "${azurerm_virtual_network.demo.name}"
+  resource_group_name  = "azurerm_resource_group.demo.name"
+  address_prefix       = "10.0.1.0/24"
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
   virtual_network_name = "${azurerm_virtual_network.demo.name}"
   resource_group_name  = "${azurerm_resource_group.demo.name}"
-  address_prefix       = "10.0.1.0/24"
+  address_prefix       = "10.0.2.0/24"
 }
