6f0bc8475a52dbaa88dff209033b7c7f7ce34615
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-10 14:15:56 -0500

    added compute section

--- compute/main.tf
+++ compute/main.tf
@@ -121,10 +121,10 @@ resource "azurerm_virtual_machine" "myterraformvm" {
 
     os_profile_linux_config {
         disable_password_authentication = true
-        #ssh_keys {
-           #path     = "/home/azureuser/.ssh/authorized_keys"
-           # key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
-        #}
+        ssh_keys {
+           path     = "/home/azureuser/.ssh/authorized_keys"
+           key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
+        }
     }
 
     tags = {
