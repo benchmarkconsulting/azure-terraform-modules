d8b3d68ab0990d85a1cf3343f766b35b9a4a3f07
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-09 20:14:47 -0500

    added vnet code

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -12,3 +12,13 @@ variable "address_space" {
   type        = string
   description = "Total network address space"
 }
+
+variable "azurerm_resource_group" {
+  type = string
+  description = "Resource Group"
+}
+
+variable "azurerm_virtual_network" {
+  type = string
+  description = "Virtual Network"
+}
