cb8056ca6dd400eb0bb734c065b6776686974b42
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-10 13:08:23 -0500

    added compute section

--- compute/main.tf
+++ compute/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "nsg" {
 
 resource "azurerm_virtual_machine" "vm-linux" {
   #count                        = "${!contains(list("${var.vm_os_simple}","${var.vm_os_offer}"), "Windows") && var.is_windows_image != "true" && var.data_disk == "false" ? var.nb_instances : 0}"
-  name                          = "${var.vm_hostname}${count.index}"
+  name                          = var.name
   location                      = var.location
   resource_group_name           = var.resource_group_name
   #availability_set_id          = azurerm_availability_set.vm.id
