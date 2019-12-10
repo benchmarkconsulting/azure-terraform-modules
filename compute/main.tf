41ca7c0ef34fd708a206629c6a90e8e9081c5ef6
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-10 13:30:18 -0500

    added compute section

--- compute/main.tf
+++ compute/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "nsg" {
 }
 
 resource "azurerm_network_interface" "main" {
-  nic_name            = var.nic
+  name            = var.nic
   location            = var.location
   resource_group_name = var.resource_group_name
 
