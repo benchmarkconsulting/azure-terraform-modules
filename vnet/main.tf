71aa3787c9b3dc5d952426f131b44e19595055e9
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-09 21:56:23 -0500

    added vnet code

--- vnet/main.tf
+++ vnet/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "network" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  location            = var.location}
+  location            = var.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.network.name
   dns_servers         = var.dns_servers
