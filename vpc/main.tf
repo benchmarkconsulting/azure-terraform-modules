29fbb1fac62bd230796103b60842863a97481933
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-12-09 20:08:25 -0500

    updated module

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,11 +1,8 @@
 resource "azurerm_virtual_network" "vpc-network" {
+  #count            = var
   name             = var.network_name
   address_space    = [var.address_space]
   resource_group   = var.resource_group 
   location         = var.location
-
-  subnet {
-    name         = var.subnet[count.index]["subnet_name"]
-    address_prefix = var.subnet[count.index]["subnet_ip"]
   }
 }
