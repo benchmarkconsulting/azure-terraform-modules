1c277d435f8c00bd0bccf1a3cf9df7d37538cb9b
Jeremy Carson <jeremycarson88@gmail.com>
2019-12-10 11:19:20 -0500

    added compute section

--- compute/main.tf
+++ compute/main.tf
@@ -6,19 +6,19 @@ resource "azurerm_resource_group" "nsg" {
 resource "azurerm_virtual_machine" "vm-linux" {
   #count                         = "${!contains(list("${var.vm_os_simple}","${var.vm_os_offer}"), "Windows") && var.is_windows_image != "true" && var.data_disk == "false" ? var.nb_instances : 0}"
   name                          = "${var.vm_hostname}${count.index}"
-  location                      = "${var.location}"
-  resource_group_name           = "${azurerm_resource_group.vm.name}"
-  availability_set_id           = "${azurerm_availability_set.vm.id}"
-  vm_size                       = "${var.vm_size}"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.vm.name
+  availability_set_id           = azurerm_availability_set.vm.id
+  vm_size                       = var.vm_size
   network_interface_ids         = ["${element(azurerm_network_interface.vm.*.id, count.index)}"]
-  delete_os_disk_on_termination = "${var.delete_os_disk_on_termination}"
+  delete_os_disk_on_termination = var.delete_os_disk_on_termination
 
   storage_image_reference {
-    id        = "${var.vm_os_id}"
-    publisher = "${var.vm_os_id == "" ? coalesce(var.vm_os_publisher, module.os.calculated_value_os_publisher) : ""}"
-    offer     = "${var.vm_os_id == "" ? coalesce(var.vm_os_offer, module.os.calculated_value_os_offer) : ""}"
-    sku       = "${var.vm_os_id == "" ? coalesce(var.vm_os_sku, module.os.calculated_value_os_sku) : ""}"
-    version   = "${var.vm_os_id == "" ? var.vm_os_version : ""}"
+    id        = var.vm_os_id
+    publisher = var.vm_os_id == "" ? coalesce(var.vm_os_publisher, module.os.calculated_value_os_publisher) : ""
+    offer     = var.vm_os_id == "" ? coalesce(var.vm_os_offer, module.os.calculated_value_os_offer) : ""
+    sku       = var.vm_os_id == "" ? coalesce(var.vm_os_sku, module.os.calculated_value_os_sku) : ""
+    version   = var.vm_os_id == "" ? var.vm_os_version : ""
   }
 
   storage_os_disk {
