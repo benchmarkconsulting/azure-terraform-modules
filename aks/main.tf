a0bd45b75bcc9064430f203c69fa7efa43abd269
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-12-11 13:59:21 -0500

    Added AKS modeule code

--- aks/main.tf
+++ aks/main.tf
@@ -13,6 +13,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name       = var.node_name
     node_count = var.node_count
     vm_size    = var.node_size
+    type       = var.node_type
+    min_count  = var.min_node_count
+    max_count  = var.max_node_count
+    max_pods   = var.max_pods
   }
 
   service_principal {
--- aks/variables.tf
+++ aks/variables.tf
@@ -20,6 +20,20 @@ variable "resource_group_name" {
   description = "Resource group to add AKS cluter to"
 }
 
+variable "min_node_count" {
+  type = string
+  description = "Minimum number of nodes for auto-scaling"
+}
+
+variable "max_node_count" {
+  type = string
+  description = "Maximum number of nodes for auto-scaling"
+}
+
+variable "max_pods" {
+  type = string
+  description = "max number of pods per node"
+}
 variable "location" {
   type = string
   description = "what region shoudl we add the cluster to"
