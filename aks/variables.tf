dc3df0aca0dbdc5bfdc59a43a8291dddcaf6e043
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-12-11 22:00:07 -0500

    remove fefalt nose pool

--- aks/variables.tf
+++ aks/variables.tf
@@ -1,14 +1,14 @@
-variable "node_type" {
-  type = string
-  description = "Availabilityset or VirtualMachineScaleSets"
-  default = "VirtualMachineScaleSets"
-}
+# variable "node_type" {
+#   type = string
+#   description = "Availabilityset or VirtualMachineScaleSets"
+#   default = "VirtualMachineScaleSets"
+# }
 
-variable "autoscale" {
-  type = string
-  description = "Availabilityset or VirtualMachineScaleSets"
-  default = "true"
-}
+# variable "autoscale" {
+#   type = string
+#   description = "Availabilityset or VirtualMachineScaleSets"
+#   default = "true"
+# }
 
 variable "cluster_name" {
   type = string
@@ -20,39 +20,39 @@ variable "resource_group_name" {
   description = "Resource group to add AKS cluter to"
 }
 
-variable "min_node_count" {
-  type = string
-  description = "Minimum number of nodes for auto-scaling"
-}
+# variable "min_node_count" {
+#   type = string
+#   description = "Minimum number of nodes for auto-scaling"
+# }
 
-variable "max_node_count" {
-  type = string
-  description = "Maximum number of nodes for auto-scaling"
-}
+# variable "max_node_count" {
+#   type = string
+#   description = "Maximum number of nodes for auto-scaling"
+# }
 
-variable "max_pods" {
-  type = string
-  description = "max number of pods per node"
-}
+# variable "max_pods" {
+#   type = string
+#   description = "max number of pods per node"
+# }
 variable "location" {
   type = string
   description = "what region shoudl we add the cluster to"
 }
 
-variable "node_size" {
-  type = string
-  description = "specify the image type"
-}
+# variable "node_size" {
+#   type = string
+#   description = "specify the image type"
+# }
 
-variable "node_count" {
-  type = string
-  description = "specify the number of nodes"
-}
+# variable "node_count" {
+#   type = string
+#   description = "specify the number of nodes"
+# }
 
-variable "node_name" {
-  type = string
-  description = "specify the node names"
-}
+# variable "node_name" {
+#   type = string
+#   description = "specify the node names"
+# }
 
 variable "tags" {
   type = string
